54fb39a9a1deea05395312011d896e337e2be4f2
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-05-08 13:33:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,12 @@ import os
 
 
 class Config:
-    APP_ID = int(os.environ.get("APP_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL"))
-    MONGODB_URL = os.environ.get("MONGODB_URL")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    APP_ID = int(os.environ.get("APP_ID", "10471716"))
+    API_HASH = os.environ.get("API_HASH","f8a1b21a13af154596e2ff5bed164860")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6999401413:AAHgF1ZpUsCT5MgWX1Wky7GbegyeHvzi2AU")
+    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL", "-1002048766471"))
+    MONGODB_URL = os.environ.get("MONGODB_URL", "mongodb+srv://appuz:chrijismiappuz@cluster0.yngvhc2.mongodb.net/?retryWrites=true&w=majority")
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", "6883997969"))
     DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/Downloaded"
     THUMB_LOCATION = f"{os.path.dirname(__file__)}/Thumbnails"
     TG_MAX_SIZE = 2097152000
